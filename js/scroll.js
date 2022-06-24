b1253c7bfe49f38ae565a92cfc3411b734301e40
sangjin Park <sahatk@naver.com>
2022-06-24 16:29:24 +0900

    2022-06-24 first commit

--- js/scroll.js
+++ js/scroll.js
@@ -6,6 +6,8 @@ window.onscroll = headerbarToggle;
 function headerbarToggle() {
   let style1 = `color:black;`;
   let style2 = 'color:white;';
+  let global_white = `background-image:url('img/main_img/header_box/global_icon.png')`;
+  let global_black = `background-image:url('img/main_img/header_box/global_icon_black.png')`;
   const headerBox = document.querySelector("#header_box");
   const headerNavigation = headerBox.querySelector(".header_nav");
   const headerUl = headerNavigation.querySelector("ul");
@@ -17,11 +19,14 @@ function headerbarToggle() {
     headerLi.forEach(function(el){
       el.style = style1;
     });
+    headerLi[6].style = global_black;
+
   } else {
     headerBox.style.backgroundColor = 'transparent';
     headerLi.forEach(el => {
       el.style = style2;
     });
+    headerLi[6].style = global_white;
   }
 }
 
