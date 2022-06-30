df9018a6d4d875cb28d3f4f5a76a7836a2c07427
sangjin Park <sahatk@naver.com>
2022-07-01 03:34:30 +0900

    2022-07-01 first commit

--- js/scroll.js
+++ js/scroll.js
@@ -9,8 +9,8 @@ function headerbarToggle() {
   let global_black = `background-image:url('img/main_img/header_box/global_icon_black.png')`;
   const headerBox = document.querySelector("#header_box");
   const headerNavigation = headerBox.querySelector(".header_nav");
-  const headerUl = headerNavigation.querySelector("ul");
-  const headerLi = headerUl.querySelectorAll("li");
+  const headerUl = headerNavigation.querySelector(".main_menu");
+  const headerLi = headerUl.querySelectorAll(".main_li");
   const currentScrollpos = window.pageYOffset;
   if (prevScrollpos < currentScrollpos) {
     headerBox.style.backgroundColor = 'white';
