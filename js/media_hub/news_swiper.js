c44422d278ca4b98f4502d26968d51b9dbd7bc03
sangjin Park <sahatk@naver.com>
2022-09-18 18:53:34 +0900

    2022-09-18

---
+++ js/media_hub/news_swiper.js
@@ -0,0 +1,14 @@
+const snsSwiper = new Swiper('.swiper-container-news', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    speed:500,
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next-news',
+        prevEl: '.swiper-button-prev-news',
+    }
+});
