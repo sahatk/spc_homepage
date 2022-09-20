5e88b0d2fb5a58b6a18e99b6fc13ea49a9922212
sangjin Park <sahatk@naver.com>
2022-09-20 17:27:32 +0900

    2022-09-20

--- js/media_hub/youtube_swiper.js
+++ js/media_hub/youtube_swiper.js
@@ -4,11 +4,103 @@ const youtubeSwiper = new Swiper('.swiper-container-youtube', {
     slidesPerGroup: 1,
     speed:500,
     loop: true,
-    // autoplay: {
-    //     delay: 2000,
-    // },
+    autoplay: {
+        delay: 2000,
+    },
     navigation: {
         nextEl: '.swiper-button-next-youtube',
         prevEl: '.swiper-button-prev-youtube',
     }
 });
+
+const spcSwiper = new Swiper('.swiper-container-spc', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    speed:500,
+    loop: true,
+    // autoplay: {
+    //     delay: 2000,
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next-spc',
+        prevEl: '.swiper-button-prev-spc',
+    }
+});
+
+const samlipSwiper = new Swiper('.swiper-container-samlip', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    speed:500,
+    loop: true,
+    // autoplay: {
+    //     delay: 2000,
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next-samlip',
+        prevEl: '.swiper-button-prev-samlip',
+    }
+});
+
+const parisSwiper = new Swiper('.swiper-container-paris', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    speed:500,
+    loop: true,
+    // autoplay: {
+    //     delay: 2000,
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next-paris',
+        prevEl: '.swiper-button-prev-paris',
+    }
+});
+
+const baskinSwiper = new Swiper('.swiper-container-baskin', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    speed:500,
+    loop: true,
+    // autoplay: {
+    //     delay: 2000,
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next-baskin',
+        prevEl: '.swiper-button-prev-baskin',
+    }
+});
+
+const dunkinSwiper = new Swiper('.swiper-container-dunkin', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    speed:500,
+    loop: true,
+    // autoplay: {
+    //     delay: 2000,
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next-dunkin',
+        prevEl: '.swiper-button-prev-dunkin',
+    }
+});
+
+const shakeSwiper = new Swiper('.swiper-container-shake', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    speed:500,
+    loop: true,
+    // autoplay: {
+    //     delay: 2000,
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next-shake',
+        prevEl: '.swiper-button-prev-shake',
+    }
+});
+
+
