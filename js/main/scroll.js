066729734a1e59ce21564242a9943d8d2fa9affd
sangjin Park <sahatk@naver.com>
2022-09-23 00:37:20 +0900

    2022-09-23

--- js/main/scroll.js
+++ js/main/scroll.js
@@ -5,10 +5,10 @@ window.onscroll = headerbarToggle;
 function headerbarToggle() {
   let style1 = `color:black;`;
   let style2 = 'color:white;';
-  let global_white = `background-image:url('img/main_img/header_box/global_icon.png')`;
+  let global_white = `background-image:url('img/main_img/header_box/global_icon_white.png')`;
   let global_black = `background-image:url('img/main_img/header_box/global_icon_black.png')`;
-  let logo_white = `background-image:url('img/main_img/header_box/gnb_logo_newwhite.png')`;
-  let logo_gray = `background-image:url('img/main_img/header_box/gnb_logo_new.png')`;
+  let logo_white = `background-image:url('img/main_img/header_box/gnb_logo_white.png')`;
+  let logo_gray = `background-image:url('img/main_img/header_box/gnb_logo_gray.png')`;
   const headerBox = document.querySelector("#header_box");
   const logo = document.querySelector(".logo_img");
   const headerNavigation = headerBox.querySelector(".header_nav");
@@ -20,11 +20,11 @@ function headerbarToggle() {
     headerLi.forEach(function (el) {
       el.style = style1;
     });
-    headerLi[6].style = global_black;
-    logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_new.png')`;
+    headerLi[5].style = global_black;
+    logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_gray.png')`;
 
     headerBox.addEventListener('mouseover', function () {
-      headerLi[6].style = global_black;
+      headerLi[5].style = global_black;
     });
 
   } else {
@@ -32,30 +32,30 @@ function headerbarToggle() {
     headerLi.forEach(el => {
       el.style = style2;
     });
-    headerLi[6].style = global_white;
-    logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_newwhite.png')`;
+    headerLi[5].style = global_white;
+    logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_white.png')`;
   }
 
     /*스크롤 0 일떄 headerbox 오버시 로고 이미지 변경 */
   if (currentScrollpos == 0) {
     headerBox.addEventListener('mouseover', function () {
-      logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_new.png')`;
-      headerLi[6].style = global_black;
+      logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_gray.png')`;
+      headerLi[5].style = global_black;
     });
 
     headerBox.addEventListener('mouseleave', function(){
-      logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_newwhite.png')`;
-      headerLi[6].style = global_white;
+      logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_white.png')`;
+      headerLi[5].style = global_white;
     });
   } else {
     headerBox.addEventListener('mouseover', function () {
-      logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_new.png')`;
-      headerLi[6].style = global_black;
+      logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_gray.png')`;
+      headerLi[5].style = global_black;
     });
 
     headerBox.addEventListener('mouseleave', function(){
-      logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_new.png')`;
-      headerLi[6].style = global_black;
+      logo.style.backgroundImage = `url('img/main_img/header_box/gnb_logo_gray.png')`;
+      headerLi[5].style = global_black;
     });
 
   }
