ba2ee5275e35157d45f621596533f31371f3a8d2
sangjin Park <sahatk@naver.com>
2022-09-24 21:23:46 +0900

    2022-09-24

--- js/common/global_button.js
+++ js/common/global_button.js
@@ -5,7 +5,7 @@
     
     const languageOptionComp = languageComp.querySelector(".scroll_language"); // 스크롤 언어 선택 전체 영역
 
-    
+    const logo = languageComp.querySelector(".global_logo");
     /////////////////////// 함수 ///////////////////////
     
     // const mouseoverHandler = () => {
@@ -13,17 +13,19 @@
     // }; // global로고 mouseover시 스크롤 영역 보이기 함수
     
     const mouseoutHandler = () => {
-        languageOptionComp.classList.remove('scroll_language_block');
+        languageOptionComp.classList.remove('scroll_language_block');     
     }; // global로고 mouseover시 스크롤 영역 숨기기 함수
 
     const mouseoverHandler = () => {
-        languageOptionComp.classList.add('scroll_language_block');
+        languageOptionComp.classList.add('scroll_language_block');        
     }; // global로고 mouseover시 스크롤 영역 숨기기 함수
     
 
     ////////////////////// 기능 수행 ///////////////////////
     languageComp.addEventListener("mouseover", mouseoverHandler);
-    languageOptionComp.addEventListener("mouseleave", mouseoutHandler);
+    languageComp.addEventListener("mouseleave", mouseoutHandler);
+    // logo.addEventListener("mouseleave", mouseoutHandler);
+    
     
 }
     
