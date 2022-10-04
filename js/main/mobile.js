7facd70ef3bdbcb2ad3c22175301259806b53cdb
sangjin Park <sahatk@naver.com>
2022-10-04 19:02:35 +0900

    2022-10-04

--- js/main/mobile.js
+++ js/main/mobile.js
@@ -3,7 +3,6 @@ const ham = document.querySelector(".mobile_nav");
 const mobileMenu = document.querySelector(".mobile_hover_box");
 
     ham.addEventListener("click",function(){
-        mobileMenu.style.height='300px';
-        mobileMenu.style.opacity='1';
+        mobileMenu.classList.toggle("block");
     })
 }
